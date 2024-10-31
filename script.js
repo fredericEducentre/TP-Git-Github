fc8ce61744512e06f7f47763cea8421f80b5ab2d
faical <mnafegfaical@gmail.com>
2024-10-31 15:45:03 +0100

    Revert "feat : add dynamic notes from js and form input in index"
    This reverts commit be2c88a9afc1d4a21627721367e7cc27f44accc4.

--- script.js
+++ script.js
@@ -1,53 +1,20 @@
-function displayStoredInfos() {
+let notes = [
+    "Ranger ma chambre",
+    "Faire la vaisselle",
+    "Aller à la salle de sport"
+]
+
+function displayNotes(array) {
     const infoList = document.getElementById('infoList');
     infoList.innerHTML = '';
-    let storedInfos = JSON.parse(localStorage.getItem('infos')) || [];
-    storedInfos.forEach(function (info, index) {
+
+    array.forEach(note => {
     const noteDiv = document.createElement('div');
     noteDiv.className = 'note';
-    noteDiv.textContent = info;
+    noteDiv.textContent = note;
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Supprimer';
-    deleteButton.addEventListener('click', function () {
-            deleteInfo(index);
-        });
-
-        noteDiv.appendChild(deleteButton);
-        infoList.appendChild(noteDiv);
+    infoList.appendChild(noteDiv);
     });
 }
 
-function deleteInfo(index) {
-    let storedInfos = JSON.parse(localStorage.getItem('infos')) || [];
-    storedInfos.splice(index, 1);
-    localStorage.setItem('infos', JSON.stringify(storedInfos));
-    displayStoredInfos();
-}
-
-displayStoredInfos();
-
-document.getElementById('addNote').addEventListener('click', function () {
-    const info = document.getElementById('infoInput').value;
-    if (info) {
-        let storedInfos = JSON.parse(localStorage.getItem('infos')) || [];
-        storedInfos.push(info);
-        localStorage.setItem('infos', JSON.stringify(storedInfos));
-        document.getElementById('infoInput').value = '';
-        displayStoredInfos();
-    } else {
-        alert('Veuillez entrer une note.');
-    }
-});
-
-function deleteAllInfos() {
-    localStorage.removeItem('infos');
-    displayStoredInfos();
-}
-
-document.getElementById('deleteAllButton').addEventListener('click', function () {
-    if (confirm('Êtes-vous sûr de vouloir supprimer toutes les notes ?')) {
-        deleteAllInfos();
-        document.getElementById('infoInput').value = '';
-    }
-});
+displayNotes(notes);
